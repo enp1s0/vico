905d43da9ecf71c98d38ef3a2cbb15ff11d86c2b
mutsuki <mutsuki@momo86.net>
2020-12-28 23:48:12 +0900

    Add epilogue and prologue

---
+++ utils/epilogue.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+VICO_STATE_FILE_PATH=$HOME/.vico_state
+echo "[END] Job $1 @ $(date)" >> $VICO_STATE_FILE_PATH
---
+++ utils/prologue.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+VICO_STATE_FILE_PATH=$HOME/.vico_state
+echo "[START] Job $1 @ $(date)" >> $VICO_STATE_FILE_PATH
